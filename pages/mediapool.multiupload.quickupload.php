9b987f5f13ffb4af3a949649259b6dc176171c4c
Christian Kolloch <kolloch.c@idowapro.de>
2016-12-07 10:51:12 +0100

    add multi upload to medienpool

---
+++ pages/mediapool.multiupload.quickupload.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * multiupload Addon.
+ * @author Friends Of REDAXO
+ * @package redaxo
+ * @var rex_addon $this
+ */
+
+
+
+$upload = new rex_mediapool_multiupload;
+$upload->setCallback("complete", "multiuploadEditFile");
+echo $upload->createUploadForm();
+
--- pages/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kolloch
- * Date: 07.12.16
- * Time: 09:45
- */
-
-echo 'foo';
